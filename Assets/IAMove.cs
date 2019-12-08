43ff79ef01c79c89331dddc83e665701e18bbf81
Jorge Michele Hernandez Bucci <zangetsujm@gmail.com>
2019-12-08 13:20:49 -0500

    nav mesh examples

---
+++ Assets/IAMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IAMove : MonoBehaviour {
+    NavMeshAgent agent;
+    GameObject Player;
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        Player = GameObject.FindGameObjectWithTag("Player");
+        
+    }
+	
+    void Update()
+    {
+        if(Vector3.Distance(transform.position,Player.transform.position) > agent.stoppingDistance)
+            agent.destination = Player.transform.position;
+    }
+
+}
